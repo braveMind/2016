4b164887460d7330b4ce7ae930cad6b3b2e0cd36
jun <1042439471@qq.com>
2016-04-06 13:47:02 +0800

    轻量级响应式框架grid

--- grid/Gruntfile.js
+++ grid/Gruntfile.js
@@ -3,9 +3,12 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
         cssmin: {
             grid: { src: 'src/grid.css', dest: 'dist/grid.min.css' },
+            normalize: { src: 'src/normalize.css', dest: 'dist/normalize.min.css' }
+
+
         }
     });
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.registerTask(
-        'default', ['cssmin:grid','cssmin:grid']);
+        'default', ['cssmin:grid','cssmin:grid'],['cssmin:normalize','cssmin:normalize']);
 };
